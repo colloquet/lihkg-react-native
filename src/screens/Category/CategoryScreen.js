b3b998531a4b96b8debf22bf0eff9e2c54309cde
Colloque Tsui <colloquet@gmail.com>
2018-03-19 10:00:23 +0800

    linked react-native-vector-icon

--- src/screens/Category/CategoryScreen.js
+++ src/screens/Category/CategoryScreen.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
 import { View, Text } from 'react-native'
 import { connect } from 'react-redux'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 const mapDispatch = dispatch => ({
   fetchThreadList: () => dispatch.category.fetchThreadList(),
@@ -15,6 +16,7 @@ class CategoryScreen extends PureComponent {
     return (
       <View>
         <Text>Category</Text>
+        <Icon name="rocket" size={30} color="#900" />
       </View>
     )
   }
